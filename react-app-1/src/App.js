361cdc870bce112e8c5e33bc546c689f72485bb7
caplanmaor <caplanmaor@gmail.com>
2022-04-01 11:56:13 +0300

    admin fix

--- react-app-1/src/App.js
+++ react-app-1/src/App.js
@@ -2,8 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function App() {
+  let navi = useNavigate();
+  function handleClick() {
+    navi("http://localhost:3000/admin");
+  }
   return (
     <div className="App">
       <Navbar />
